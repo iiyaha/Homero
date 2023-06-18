96b3c1998f87745a132173e0b865c998fb47ea0b
fumiya-kato <xdoreiku@yahoo.co.jp>
2023-06-18 14:19:34 +0900

    BaseUserDAO

--- dao/BaseUserDAO.java
+++ dao/BaseUserDAO.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class BaseUserDAO {
-	private final String DB_URL = "";
-	private final String DB_USER = "";
-	private final String DB_PASS = "";
+	private final String DB_URL = "";	// 接続先DB
+	private final String DB_USER = "";	// ユーザー名
+	private final String DB_PASS = "";	// パスワード
 	
 	protected Connection getConnection() throws SQLException{
 		Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
